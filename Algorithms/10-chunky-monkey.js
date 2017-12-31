c635e13fd7284035e3ffd8b4d75f6b4621a288a1
JosephWeaver <joe.w.weaver@gmail.com>
2017-12-30 16:09:28 -0800

    freeCodeCamp - Basic Algorithm Scripting
    Next five of the Basic Algorithm Scripting challenges on freeCodeCamp.

---
+++ Algorithms/10-chunky-monkey.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.org/challenges/chunky-monkey
+
+function chunkArrayInGroups(arr, size) {
+  var newArr = [];
+  for(var i = 0; i < arr.length; i+=size){
+    newArr.push(arr.slice(i, size + i));
+  }
+  return newArr;
+}
+chunkArrayInGroups(["a", "b", "c", "d"], 2); // [["a","b"],["c","d"]]
+chunkArrayInGroups(["a", "b", "c", "d", "e", "f"], 3); // [["a","b","c"],["d","e","f"]]
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # freeCodeCamp
+
+### Algorithms
+
+- 01 Reverse a String ✅
+- 02 Factorialize a Number ✅
+- 03 Check for Palindromes ✅
+- 04 Find the Longest Word in a String ✅
+- 05 Title Case a Sentence ✅
+- 06 Return Largest Numbers in Arrays ✅
+- 07 Confirm the Ending ✅
+- 08 Repeat a String ✅
+- 09 Truncate a String ✅
+- 10 Chunky Monkey ✅
