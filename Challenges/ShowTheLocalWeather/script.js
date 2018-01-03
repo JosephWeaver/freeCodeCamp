6b02d798d9ee0152efa9bf7664a601b4add3ac5f
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-03 05:13:10 -0800

    Updated Weather app JavaScript

--- Challenges/ShowTheLocalWeather/script.js
+++ Challenges/ShowTheLocalWeather/script.js
@@ -1,7 +1,8 @@
 // https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js
 
 $(function(){
+
+  // geolocation stuff
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       queryAPI(position.coords.latitude, position.coords.longitude);
@@ -9,29 +10,41 @@ $(function(){
   } else {
     alert("No geolocation available. Sorry!");
   }
+
+  // API query stuff
   function queryAPI(lat, long){
     $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+long, function(data) {
-      $("#temp .c").html(Math.round(data.main.temp * 10 ) / 10);
-      $("#temp .f").html(Math.round(((Math.round(data.main.temp * 10 ) / 10) * 9 / 5 + 32) * 10 ) / 10);
-      $("#humidity span").html(data.main.humidity);
-      $("#icon span").html(data.weather[0].description);
-      $("#location").html(data.name.split(" ").join("<br>"));
-      $("#wind span").html(data.wind.speed);
-      if(data.name.includes("San Francisco")){ $("body").addClass("sf"); }
+      displayData(data);
     });
   }
-  function parseIcon(icon) {
-    switch (icon) {
-      case 'drizzle':     setIcon(icon); break;
-      case 'clouds':      setIcon(icon); break;
-      case 'rain':        setIcon(icon); break;
-      case 'snow':        setIcon(icon); break;
-      case 'clear':       setIcon(icon); break;
-      case 'thunderstom': setIcon(icon); break;
-      default: $('div.clouds').removeClass('hide');
-    }
+
+  // display data to the page
+  function displayData(data){
+
+    // set vars
+    var tempC = Math.round(data.main.temp * 10 ) / 10;
+    var tempF = Math.round(((tempC) * 9 / 5 + 32) * 10 ) / 10;
+    var location = data.name;
+    var humid = data.main.humidity;
+    var icon = data.weather[0].main;
+    var wind = data.wind.speed;
+    var sunrise = data.sys.sunrise;
+    var sunset = data.sys.sunset;
+    var now = Math.round(Date.now() / 1000);
+    var timeOfDay = now < sunrise && now > sunset ? "night" : "day";
+
+    // push API content to elements on page
+    $("#temp .c").html(tempC);
+    $("#temp .f").html(tempF);
+    $("#humidity span").html(humid);
+    $("#wind span").html(data.wind.speed);
+    $("#icon").html("<i class='wi wi-" + timeOfDay + "-" + icon.toLowerCase() + "'></i> " + icon);
+    $("#location").html(data.name.split(" ").join("<br>"));
+    if(location.includes("San Francisco")){ $("body").addClass("sf"); }
   }
-  function setIcon(icon) {
-    $('div.' + icon).removeClass('hide');
-  }
+
+  // change fahrenheit to celsius and vice versa
+  $("#f").click(function(){ $(this).hide(); $("#c").show(); });
+  $("#c").click(function(){ $(this).hide(); $("#f").show(); });
+
 });
