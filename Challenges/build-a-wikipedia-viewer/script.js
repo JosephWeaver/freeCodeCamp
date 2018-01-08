cbcd67248dca35c78ed8c93eb8d1b28346688445
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-07 21:51:33 -0800

    Updated JavaScript for Wikipedia viewer

--- Challenges/build-a-wikipedia-viewer/script.js
+++ Challenges/build-a-wikipedia-viewer/script.js
@@ -7,98 +7,143 @@
 // 2. FIX SEARCH KEYWORD HIGHLIGHT SO IT:
 //    A. WORKS PROPERLY, AND
 //    B. DOESN'T OVERFLOW BUFFER 😒
-// 3. FIX IT SO RESULTS END UP IN THE PROPER ORDER! (i.e. "Kylo Ren" should show up FIRST)
+// 3. FIX IT SO RESULTS END UP IN THE PROPER ORDER!
 // 4. POLISH OFF STUFF SO IT LOOKS EVEN BETTER!! 😎
 
-$(()=>{ // jQuery document ready
+$(()=>{ // jQuery document.ready
 
-  var articles = $("#articles ul"),
-      button = $("button"),
-      input = $("input"),
-      random = $("#random"),
-      search = $("#search"),
-      Wikipedia = "https://en.wikipedia.org",
-      WP_API = Wikipedia + "/w/api.php";
+  const articles = $("#articles ul"),
+        input = $("input"),
+        random = $("#random"),
+        search = $("#search"),
+        Wikipedia = "https://en.wikipedia.org",
+        WP_API = Wikipedia + "/w/api.php";
 
-  var queryAPI = searchTerms => {
+  const queryAPI = terms => {
     $.ajax({
       url: WP_API,
+      // url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + input.val() + "&format=json&srprop=snippet&prop=info&inprop=url&srlimit=1",
       dataType: "jsonp",
+      // data: {
+      //   action: "query",
+      //   format: "json",
+      //   list: "search",
+      //   utf8: 1,
+      //   srsearch: input.val(),
+      //   origin: "*",
+      //   srlimit: 5
+      // },
       data: {
         action: "query",
-        format: "json",
-        generator: "search",
-        gsrsearch: searchTerms,
-        gsrnamespace: 0,
-        gsrlimit: 1,
-        prop: "extracts|pageimages",
-        exchars: 484,
+        exchars: 455,
+        exintro: true,
         exlimit: "max",
         explaintext: true,
-        exintro: true,
+        format: "json",
+        generator: "search",
+        gsrlimit: 1,
+        gsrnamespace: 0,
+        gsrsearch: terms,
         pilimit: "max",
         piprop: "thumbnail",
-        pithumbsize: 256
+        pithumbsize: 256,
+        prop: "extracts|pageimages"
       },
       success: data => {
-        var results = data.query.pages;
-        $.map(results, result => {
-          var el = $('<li>');
+        $.map(data.query.pages, result => {
+          // if highlight didn't maintain proper case, split() and join() on input.val() would work
+//           var titleMatch = result.title.toLowerCase().indexOf(input.val().toLowerCase()),
+//               titleHighlight = titleMatch !== -1 ?                                                   // if a string match exists, highlight it
+//                 result.title.substr(0, titleMatch) + "<span class='searchmatch'>" +                  // before match start + start searchmatch
+//                 result.title.substr(titleMatch, input.val().length()) + "</span>" +                  // part which matches + ends searchmatch
+//                 result.title.substr(titleMatch + input.val().length(), result.title.length()) : "",  // after match ends + closes out ternary
+//               descrMatch = result.extract.toLowerCase().indexOf(input.val().toLowerCase()),
+//               descrHighlight = descrMatch !== -1 ?                                                       // if a string match exists, highlight it
+//                 result.extract.substr(0, descrMatch) + "<span class='searchmatch'>" +                    // before match start + start searchmatch
+//                 result.extract.substr(descrMatch, input.val().length()) + "</span>" +                    // part which matches + ends searchmatch
+//                 result.extract.substr(descrMatch + input.val().length(), result.extract.length()) : "";  // after match ends + closes out ternary
 
-//           console.log("original title: "+result.title);
-//           console.extract("original title: "+result.extract);
-//           var newTitle = result.title.split(input.val()).join("<span class='searchmatch'>"+input.val()+"</span>");
+//           console.log("inputVal: " + input.val());
+//           console.log("title: " + result.title);
+//           console.log("extract: " + result.extract);
+//           console.log("highlight: " + titleHighlight);
+//           console.log("descr: " + descrHighlight);
 
-//           var newExtract = result.extract.split(input.val()).join("<span class='searchmatch'>"+input.val()+"</span>");
-//           console.log("new title: "+newTitle);
-//           console.extract("new title: "+newExtract);
+          var title = result.title.split(input.val()).join("<span class='searchmatch'>" + input.val() + "</span>"),
+              descr = result.extract.split(input.val()).join("<span class='searchmatch'>" + input.val() + "</span>");
+          var li = $('<li>');
+          li.append($("<a>").attr("href", Wikipedia + "/wiki/" + result.title).attr("target", "_blank")
+            .append($("<h2>").html(title))
+            .append(result.thumbnail ? $("<img>").attr("width", 128).attr("src", result.thumbnail.source) : "")
+            .append($("<p>").html(descr)));
+
+          // $(li + ":contains(" + input.val() + ")").html((_, html) => {
+          //   return html.split(input.val()).join("<span class='searchmatch'>" + input.val() + "</span>");
+          // });
+          articles.empty().append(li).hide().fadeIn(284);
+
+          //           console.log("original title: "+result.title);
+          //           console.extract("original title: "+result.extract);
+          //           var newTitle = result.title.split(input.val()).join("<span class='searchmatch'>"+input.val()+"</span>");
+
+          //           var newExtract = result.extract.split(input.val()).join("<span class='searchmatch'>"+input.val()+"</span>");
+          //           console.log("new title: "+newTitle);
+          //           console.extract("new title: "+newExtract);
 
           // $("body *:contains("+input.val()+")").html(function(_, html) {
           //   return html.split(input.val()).join("<span class='searchmatch'>"+input.val()+"</span>");
           // });
-          el.append( $('<a>').attr('href',Wikipedia+"/wiki/"+result.title).append($('<h2>').text(result.title)).append(result.thumbnail?$('<img>').attr('width',128).attr('src',result.thumbnail.source):"").append($('<p>').text(result.extract)));
-          articles.append(el).hide().fadeIn(284);
+          // var el = $('<li>');
+          // el.append($("<a>").attr("href",Wikipedia+"/wiki/"+result.title).attr("target", "_blank").append($("<h2>").text(result.title)).append(result.thumbnail ? $("<img>").attr("width", 128).attr("src", result.thumbnail.source) : "").append($("<p>").text(result.extract)));
+          // articles.empty().append(el).hide().fadeIn(284);
         });
       }
     });
-  };
-  var queryAPIRandom = () => {
-    $.ajax({
-      url: WP_API,
-      dataType: "jsonp",
-      data: {
-        action: "query",
-        format: "json",
-        formatversion: 2,
-        list: "random",
-        rnnamespace: 0,
-        rnlimit: 5,
-        titles: "Main_page",
-        callback: "?"
-      },
-      success: data => {
-        var results = data.query.pages;
-        $.map(results, result => {
-          var el = $('<li>');
-          el.append($('<h2>').append($('<a>').attr('href',Wikipedia+"/wiki/"+result.title).text(result.title)));
-          if (result.thumbnail) el.append($('<img>').attr('width',128).attr('src',result.thumbnail.source));
-          el.append($('<p>').text(result.extract));
-          articles.append(el).hide().fadeIn(284);
-        });
-      }
-    });
-  };
+  },
+        queryAPIRandom = () => {
+            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=5&titles=&callback=?", data => {
+              $.map(data.query.random, result => {
+                // console.log(result.title);
+                articles.empty().append($('<li>').attr("class", "random").append($("<a>").attr("href", Wikipedia + "/wiki/" + result.title).attr("target", "_blank").append($("<h2>").html(result.title)))).hide().fadeIn(284);
+              });
+            });
+
+          // $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=15&titles=&callback=?", data => {
+          //   $.map(data.query.random, result => {
+          //     articles.empty()
+          //       .append($('<li>')
+          //         .append($("<a>").attr("href", Wikipedia + "/wiki/" + result.title).attr("target", "_blank")
+          //           .append($("<h2>").html(result.title))
+          //         )
+          //       ).hide().fadeIn(284);
+          //   });
+          // });
+          // $.ajax({
+          //   url: WP_API,
+          //   dataType: "json",
+          //   data: {
+          //     action: "query",
+          //     format: "json",
+          //     formatversion: 2,
+          //     list: "random",
+          //     rnnamespace: 0,
+          //     rnlimit: 5,
+          //     titles: "",
+          //     callback: ""
+          //   },
+          //   success: data => {
+          //     // console.log(data);
+          //     $.map(data.query.random, result => {
+          //       var el = $("<li>");
+          //     el.append($("<a>").attr("href",Wikipedia+"/wiki/"+result.title).attr("target", "_blank").append($("<h2>").text(result.title)).append(result.thumbnail ? $("<img>").attr("width", 128).attr("src", result.thumbnail.source) : "").append($("<p>").text(result.extract)));
+          //       articles.empty().append(el).hide().fadeIn(284);
+          //     });
+          //   }
+          // });
+        };
 
   input.on("input", doSearch);
-  button.click(doSearch);
-  // search.on("submit", e => e.preventDefault());
-  search.on("submit", function(){ return false; });
-  random.click(e => {
-    e.preventDefault();
-    queryAPIRandom();
-  });
-  function doSearch(){
-    articles.empty();
-    queryAPI(input.val());
-  }
+  search.on("submit", e => { e.preventDefault(); doSearch(); });
+  random.click(e => { e.preventDefault(); queryAPIRandom(); });
+  function doSearch(e){  queryAPI(input.val()); }
 });
