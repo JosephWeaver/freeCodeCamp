00fdef8e1745df984dc458ac6914e1aa2f63debd
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-08 05:53:17 -0800

    Yet another version of the JS for the Wikipedia viewer

--- Challenges/build-a-wikipedia-viewer/script.js
+++ Challenges/build-a-wikipedia-viewer/script.js
@@ -24,33 +24,44 @@ $(()=>{ // jQuery document.ready
       url: WP_API,
       // url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + input.val() + "&format=json&srprop=snippet&prop=info&inprop=url&srlimit=1",
       dataType: "jsonp",
-      // data: {
-      //   action: "query",
-      //   format: "json",
-      //   list: "search",
-      //   utf8: 1,
-      //   srsearch: input.val(),
-      //   origin: "*",
-      //   srlimit: 5
-      // },
       data: {
-        action: "query",
-        exchars: 455,
-        exintro: true,
-        exlimit: "max",
-        explaintext: true,
+        // action: "query",
+        // format: "json",
+        // list: "search",
+        // utf8: 1,
+        // srsearch: input.val(),
+        // origin: "*",
+        // exchars: 455,
+        // exintro: true,
+        // exlimit: "max",
+        // explaintext: true,
         format: "json",
-        generator: "search",
-        gsrlimit: 1,
-        gsrnamespace: 0,
-        gsrsearch: terms,
-        pilimit: "max",
+        // generator: "search",
+        // gsrlimit: 15,
+        // gsrnamespace: 0,
+        // gsrsearch: terms,
+        pilimit: 10,
         piprop: "thumbnail",
         pithumbsize: 256,
-        prop: "extracts|pageimages"
+        // prop: "extracts|pageimages"
+      // },
+      // data: {
+        list: "search",
+        srsearch: terms,
+        // sroffset: 0,
+        srlimit: 10,
+        prop: "revisions|pageimages",
+        rvprop: "content"
       },
       success: data => {
-        $.map(data.query.pages, result => {
+        data.query.search.reverse();
+        // var results = data.query.pages;
+        var results = data.query.search;
+        // $(results.get()).each(result => {
+        console.clear();
+        $.map(results, result => {
+          console.log(result.title);
+
           // if highlight didn't maintain proper case, split() and join() on input.val() would work
 //           var titleMatch = result.title.toLowerCase().indexOf(input.val().toLowerCase()),
 //               titleHighlight = titleMatch !== -1 ?                                                   // if a string match exists, highlight it
@@ -70,7 +81,7 @@ $(()=>{ // jQuery document.ready
 //           console.log("descr: " + descrHighlight);
 
           var title = result.title.split(input.val()).join("<span class='searchmatch'>" + input.val() + "</span>"),
-              descr = result.extract.split(input.val()).join("<span class='searchmatch'>" + input.val() + "</span>");
+              descr = result.snippet.split(input.val()).join("<span class='searchmatch'>" + input.val() + "</span>");
           var li = $('<li>');
           li.append($("<a>").attr("href", Wikipedia + "/wiki/" + result.title).attr("target", "_blank")
             .append($("<h2>").html(title))
@@ -101,49 +112,93 @@ $(()=>{ // jQuery document.ready
     });
   },
         queryAPIRandom = () => {
-            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=5&titles=&callback=?", data => {
+            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=10&titles=&callback=?", data => {
               $.map(data.query.random, result => {
                 // console.log(result.title);
                 articles.empty().append($('<li>').attr("class", "random").append($("<a>").attr("href", Wikipedia + "/wiki/" + result.title).attr("target", "_blank").append($("<h2>").html(result.title)))).hide().fadeIn(284);
               });
             });
 
-          // $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=15&titles=&callback=?", data => {
-          //   $.map(data.query.random, result => {
-          //     articles.empty()
-          //       .append($('<li>')
-          //         .append($("<a>").attr("href", Wikipedia + "/wiki/" + result.title).attr("target", "_blank")
-          //           .append($("<h2>").html(result.title))
-          //         )
-          //       ).hide().fadeIn(284);
-          //   });
-          // });
-          // $.ajax({
-          //   url: WP_API,
-          //   dataType: "json",
-          //   data: {
-          //     action: "query",
-          //     format: "json",
-          //     formatversion: 2,
-          //     list: "random",
-          //     rnnamespace: 0,
-          //     rnlimit: 5,
-          //     titles: "",
-          //     callback: ""
-          //   },
-          //   success: data => {
-          //     // console.log(data);
-          //     $.map(data.query.random, result => {
-          //       var el = $("<li>");
-          //     el.append($("<a>").attr("href",Wikipedia+"/wiki/"+result.title).attr("target", "_blank").append($("<h2>").text(result.title)).append(result.thumbnail ? $("<img>").attr("width", 128).attr("src", result.thumbnail.source) : "").append($("<p>").text(result.extract)));
-          //       articles.empty().append(el).hide().fadeIn(284);
-          //     });
-          //   }
-          // });
+//           $.ajax({
+//             url: WP_API,
+//             dataType: "jsonp",
+//             data: {
+//               action: "query",
+//               format: "json",
+//           //     formatversion: 2,
+//               list: "random",
+//               rnnamespace: 0,
+//               rnlimit: 15,
+//               titles: "",
+//               callback: "ar"
+//             },
+//             success: data => {
+//           //     // console.log(data);
+//               $.map(data.query.random, result => {
+//                 console.log(result);
+//               //   var el = $("<li>");
+//               // el.append($("<a>").attr("href",Wikipedia+"/wiki/"+result.title).attr("target", "_blank").append($("<h2>").text(result.title)).append(result.thumbnail ? $("<img>").attr("width", 128).attr("src", result.thumbnail.source) : "").append($("<p>").text(result.extract)));
+//               //   articles.empty().append(el).hide().fadeIn(284);
+
+//                 // articles.empty().append($('<li>').attr("class", "random").append($("<a>").attr("href", Wikipedia + "/wiki/" + result.title).attr("target", "_blank").append($("<h2>").html(result.title)))).hide().fadeIn(284);
+
+//               });
+//             }
+//           });
         };
 
-  input.on("input", doSearch);
+  // input.on("input", doSearch);
+  input.on("input", searchFor(input.val()));
   search.on("submit", e => { e.preventDefault(); doSearch(); });
   random.click(e => { e.preventDefault(); queryAPIRandom(); });
   function doSearch(e){  queryAPI(input.val()); }
+  function searchFor(searchTerm){
+    $.ajax({
+      url: WP_API,
+      dataType: "jsonp",
+      data: {
+        action: "query",
+        format: "json",
+        pilimit: 10,
+        piprop: "thumbnail",
+        pithumbsize: 256,
+        prop: "extracts|pageimages",
+        list: "search",
+        srsearch: searchTerm,
+        srlimit: 10
+      },
+      success: data => data.query.search.forEach(result => extractFrom(result))
+    });
+  }
+  function extractFrom(result){
+    $.ajax({
+      url: WP_API,
+      dataType: "jsonp",
+      data: {
+        action: "query",
+        exchars: "430",
+        format: "json",
+        prop: "extracts",
+        titles: result.title
+      },
+      success: data => {
+        for (var id in data.query.pages){
+          if (data.query.pages.hasOwnProperty(id)){
+            // console.log(data.query.pages[id].title);
+            // console.log(data.query.pages[id].extract);
+            displayResults(data.query.pages[id].title, data.query.pages[id].extract);
+          }
+        }
+      }
+    });
+  }
+  function displayResults(title, extract){
+    var li = $('<li>');
+    li.append($("<a>").attr("href", Wikipedia + "/wiki/" + title).attr("target", "_blank")
+      .append($("<h2>").html(title))
+   // .append(thumbnail ? $("<img>").attr("width", 128).attr("src", result.thumbnail.source) : "")
+      .append($("<p>").html(extract)));
+    articles.empty().append(li).hide().fadeIn(284);
+  }
+
 });
