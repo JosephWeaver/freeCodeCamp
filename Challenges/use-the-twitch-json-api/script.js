2246e291dbb708269b775b83d973721e61be5d38
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-11 10:39:46 -0800

    Updated JS

--- Challenges/use-the-twitch-json-api/script.js
+++ Challenges/use-the-twitch-json-api/script.js
@@ -21,7 +21,6 @@ $(()=>{
       // define stream list, info, status
       streams = [
         "freecodecamp",
-        "sodapoppin",
         "esl_sc2",
         "day9tv"
       ],
@@ -34,7 +33,7 @@ $(()=>{
   }
 
   // update stream status every few seconds
-  // liveStreams = setInterval(getStreamData(streams, "live"), 2843);
+  // setInterval(getStreamData(streams, "live"), 2843);
 
   // nav click, input change, and search submit handlers
   $navLink.on("click", e => showStreams(e));
@@ -46,50 +45,36 @@ $(()=>{
 
   // define function to pull JSON from Twitch API
   function getStreamData(stream, live = null){
-    return live === null ?
-      $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/"+stream+"?callback=?", data => {
-      var li = $("<li>");
-      li.attr("style","background: url(" + data.stream.channel.profile_banner + ")");
-      li.attr("class","live");
-      li.append($("<div>").attr("class", "avatar").append($("<img>").attr("src", data.stream.channel.logo)))
-        .append($("<div>").attr("class", "info")
-                .append($("<a>").attr("href", "https://www.twitch.tv/" + stream).attr("target", "_blank")
-                        .append($("<div>").attr("class", "name").html(data.stream.channel.display_name))
-                        .append($("<div>").attr("class", "stream").html(data.stream.channel.display_name))))
-        .append($("<div>").attr("class", "status").html(live ? "yes" : "no"));
-      $streams.prepend(li).hide().delay().slideDown(284);
-    }) : streams.forEach(stream => {
-      $.getJSON(API_URL + "/channels/"+stream+"?callback=?", data => {
-        console.log(data.profile_banner);
-        return data.logo;
+    return live ? $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/"+stream+"?callback=?", data => {
+        let li = $("<li>");
+        li.attr("style","background: url(" + data.stream.channel.profile_banner + ")").attr("class","live").append($("<div>").attr("class", "avatar").append($("<img>").attr("src", data.stream.channel.logo))).append($("<div>").attr("class", "info").append($("<a>").attr("href", "https://www.twitch.tv/" + stream).attr("target", "_blank").append($("<div>").attr("class", "name").html(data.stream.channel.display_name)).append($("<div>").attr("class", "stream").html(data.stream.channel.display_name)))).append($("<div>").attr("class", "status").html(live ? "LIVE" : "Offline"));}) : $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/"+stream+"?callback=?", data => {
+      console.log(data);
+        let li = $("<li>");
+        li.attr("style","background: url(" + data.profile_banner + ")");
+        li.append($("<div>").attr("class", "avatar").append($("<img>").attr("src", data.logo)));
+        li.append($("<div>").attr("class", "info").append($("<a>").attr("href", "https://www.twitch.tv/" + stream).attr("target", "_blank").append($("<div>").attr("class", "name").html(data.display_name)).append($("<div>").attr("class", "stream").html(data.display_name)))).append($("<div>").attr("class", "status").html(live ? "LIVE" : "Offline"));
+        $streams.prepend(li).hide().delay().slideDown(284);
       });
-      $.getJSON(API_URL + "/streams/"+stream+"?callback=?", data => {
-        // console.log(data);
-      });
-      $.getJSON(API_URL + "/users/"+stream+"?callback=?", data => {
-        // console.log(data);
-      });
-    });
-  }
-
-  // function fired when nav links are clickedfor clicking all, live, offline
-  function showStreams(e){
-    let $clicked = $("#" + e.target.id);
-    $clicked.addClass("selected").siblings().removeClass("selected");
-    switch(e.target.id){
-      case "live": console.log("show live"); break;
-      case "offline": console.log("show offline"); break;
-      default: console.log("show all");
     }
-  }
 
-  // function for searching streamers list (extra)
-  function searchStreams(e){
-    e.preventDefault();
-    console.log($input.val());
-  }
+    // function fired when nav links are clickedfor clicking all, live, offline
+    function showStreams(e){
+      let $clicked = $("#" + e.target.id);
+      $clicked.addClass("selected").siblings().removeClass("selected");
+      switch(e.target.id){
+        case "live": console.log("show live"); break;
+        case "offline": console.log("show offline"); break;
+        default: console.log("show all");
+      }
+    }
 
-  // function for watching twitch stream (extra)
+    // function for searching streamers list (extra)
+    function searchStreams(e){
+      e.preventDefault();
+      console.log($input.val());
+    }
+
+    // function for watching twitch stream (extra)
 
 
-});
+  });
