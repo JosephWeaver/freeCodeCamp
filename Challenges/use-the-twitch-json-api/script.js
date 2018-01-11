00da41575e4240eafe2ca2449fade4f62861ba85
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-10 16:06:00 -0800

    Added JS for Twitch project

---
+++ Challenges/use-the-twitch-json-api/script.js
@@ -0,0 +1,17 @@
+$(()=>{
+
+  // define variables for DOM elements
+
+  // define variables for list of Twitch channels
+
+  // define function to pull JSON from Twitch API
+
+  // define function to poll for online vs offline
+
+  // define functions for clicking all, online, offline
+
+  // define function for searching list of streamers (extra)
+
+  // define function for watching twitch stream (extra)
+
+});
