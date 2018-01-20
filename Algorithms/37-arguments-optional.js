8920dc8933baab4bc845a696ce78511a592cf14a
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-19 23:32:13 -0800

    Finished Arguments Optional

--- Algorithms/37-arguments-optional.js
+++ Algorithms/37-arguments-optional.js
@@ -1,1 +1,8 @@
 // https://www.freecodecamp.org/challenges/arguments-optional
+
+function addTogether(){
+  var a = arguments[0];
+  var b = arguments[1];
+  return Number.isInteger(a) ? Number.isInteger(b) ? a + b : !b ? function(b){ return Number.isInteger(b) ? a + b : undefined; } : undefined : undefined;
+}
+addTogether(2,3);
