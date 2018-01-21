98d649818c8638853f6f5bf278c5e6cb1ae8c5ed
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-20 16:31:02 -0800

    Finished Validate US Telephone Numbers

--- Algorithms/38-validate-us-telephone-numbers.js
+++ Algorithms/38-validate-us-telephone-numbers.js
@@ -1,2 +1,6 @@
 // https://www.freecodecamp.org/challenges/validate-us-telephone-numbers
 
+function telephoneCheck(str){
+  return /^1?\s?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/.test(str);
+}
+telephoneCheck("555-555-5555");
