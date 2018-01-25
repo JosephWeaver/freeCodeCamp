02b1aba45b1d2937f994a42d2b34be58e2e2785b
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-25 05:25:40 -0800

    Updated JS
    Still incomplete

--- Challenges/build-a-javascript-calculator/script.js
+++ Challenges/build-a-javascript-calculator/script.js
@@ -9,29 +9,28 @@ $(()=>{
         $button = $("button")
 
   let clicked,
-      currNumber = "",
+      currentAnswer = "",
       historyString = "",
       history = [
-        {
-          type: "number",
-          value: "3"
-        },
-        {
-          type: "number",
-          value: "3"
-        },
-        {
-          type: "operator",
-          value: "*"
-        },
-        {
-          type: "number",
-          value: "3"
-        }
+        // {
+        //   type: "num",
+        //   value: "3"
+        // },
+        // {
+        //   type: "num",
+        //   value: "3"
+        // },
+        // {
+        //   type: "op",
+        //   value: "*"
+        // },
+        // {
+        //   type: "num",
+        //   value: "3"
+        // }
       ]
 
   init()
-  console.log(history)
 
   function init(){
     $button
@@ -40,88 +39,97 @@ $(()=>{
       .on("click", e => click(e.target.value))
   } // complete
   function click(clicked){
-    // getType(clicked)
-
-    for(var i = 0, len = history.length; i < len; i++){
-      makeHistory(history[i])
-    }
-
-    //     console.log(history)
-
-    //     if (getType(clicked) === "number"){
-    //       args[args.length] = args[args.length] + clicked
-    //       types.push(getType(clicked))
-    //     } else {
-    //       args.push(clicked)
-    //     }
-    //     // // types.push(getType(clicked))
-    //     // // args.push(clicked)
-    //     // // updateDisplay(clicked)
-    //     console.log(args, types)
-  }
-  function clear(which){
-    switch(which){
-      case "all":
+    if (history.length > 0){
+      let historyLength = history.length,
+          previousValue = history[historyLength].value,
+          previousType = getType(previousValue)
+      } // instantiate previous type and value (if they exist)
+    history.push({ type: getType(clicked), value: clicked }) // push clicked into history
+    updateDisplay(history) // update display with answer & history
+  } // complete
+  function clear(last = null){
+    if (last){
+      let previousType = getType(history[history.length].value)
+      if (history.length > 0 && (previousType === "num" || previousType === "dec")) {
+          while (previousType === "num" || previousType === "dec"){
+            history.pop()
+            previousType = getType(history[history.length].value)
+          }
+        } else {
+          history.pop()
+        }
+    } else {
         history = []
-        currNumber = "0"
-        historyString = "0"
-        displayAnswer(history)
-        makeHistory(history)
-        break
-      case "last":
-        history[history.length].type === "operator" ? history.pop() :
-        // currNumber = "0"
-        displayAnswer(history)
-        makeHistory(history)
-        break
     }
-  }
+    updateDisplay(history)
+  } // complete
   function getType(item){
-    switch(item.type){
+    switch(item){
+      case "c":
+        return "c"
+        break
+      case "ce":
+        return "ce"
+        break
+      case ".":
+        return "dec"
+        break
+      case "=":
+        return "eq"
+        break
+      default:
+        return "num"
       case "+":
       case "-":
       case "x":
       case "/":
-        return "operator"
+        return "op"
         break
-      case "ce":
-      case "c":
-        return "clear"
-        break
-      case ".":
-        return "decimal"
-        break
-      case "=":
-        return "equals"
-        break
-      default:
-        return "number"
     }
   } // complete
-  function makeHistory(history){
-    for (var i = 0, len = history.length; i < len; i++){
-      switch(history[i].type){
-        case "operator":
-          if (history[i - 1] > -1 && history[i - 1].type === "operator"){ history.splice(history[i], 1); }
-          historyString += " " + history[i].value;
-          break
-        case "number":
-          historyString += history[i].value;
-        default:
-          break
-      }
-    }
-
+  // function makeHistory(history){
+  //   // for (var i = 0, len = history.length; i < len; i++){
+  //   //   switch(history[i].type){
+  //   //     case "op":
+  //   //       if (history[i - 1] > -1 && history[i - 1].type === "op"){ history.splice(history[i], 1); }
+  //   //       historyString += " " + history[i].value;
+  //   //       break
+  //   //     case "num":
+  //   //       historyString += history[i].value;
+  //   //     default:
+  //   //       break
+  //   //   }
+  //   // }
+  //   // historyString = $history.text() === "0" ? "" : $history.text()
+  //   // historyString += clicked
+  //   // $history.text(historyString)
+  // }
+  function updateDisplay(history){
+    let previousType
+    switch (getType(history[history.length].value)){
+      case "eq":
+        // create answer from full history string
+        // display answer and history
+        break
+      case "c":
+        clear()
+        break
+      case "ce":
+        clear("entry")
+        break
+      case "op":
+        if (getType(history[history.length - 1].value) === "op"){
+          currentAnswer = history[history.length].value
+          history.pop().pop().push({ type: getType(currentAnswer), value: currentAnswer })
     // historyString = $history.text() === "0" ? "" : $history.text()
     // historyString += clicked
-    $history.text(historyString)
-  }
-  function showAnswer(clicked){
-    $answer.text(type === "number" ? $answer.text() + clicked : clicked)
-  }
-  function updateDisplay(clicked){
-    showAnswer(clicked)
-    makeHistory(clicked)
-  }
+    // $history.text(historyString)
+        } else {
+          historyString = history[history.length].value
+        }
+        break
+      case "num":
 
+    }
+  }
 })
