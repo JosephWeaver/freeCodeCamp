5dad2a5b26a8e4274aea9ec509a5c3fb0be5ce6b
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-26 16:04:30 -0800

    Finished 43 No Repeats Please

--- Algorithms/43-no-repeats-please.js
+++ Algorithms/43-no-repeats-please.js
@@ -1,6 +1,21 @@
 // https://www.freecodecamp.org/challenges/no-repeats-please
 
-function permAlone(str) {
-  return str;
+/* jshint esversion: 6 */ // <-- NEEDS JSHINT ESVERSION SPECIFIED AS 6 TO WORK ON FCC
+function permAlone(str){
+  function getPerms(str){
+    function *permute(a, n = a.length) {
+      if (n <= 1) yield a.slice();
+      else for (let i = 0; i < n; i++) {
+        yield *permute(a, n - 1);
+        const j = n % 2 ? 0 : i;
+        [a[n-1], a[j]] = [a[j], a[n-1]];
+      }
+    }
+    return Array.from(permute(str.split(""))).map(perm => perm.join(""));
+  }
+  var perms = getPerms(str), regex = /(.)\1+/g;
+  return perms.filter(function(string) {
+    return !string.match(regex);
+  }).length;
 }
 permAlone('aab');
