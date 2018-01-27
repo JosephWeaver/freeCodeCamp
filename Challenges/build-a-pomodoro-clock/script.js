92e4189eea0612c0f9499a54d18588bc20e1ecd3
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-27 04:15:16 -0800

    Uploaded JS notes for Build a Pomodoro Clock

---
+++ Challenges/build-a-pomodoro-clock/script.js
@@ -0,0 +1,20 @@
+// https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+
+$(()=>{
+
+  // make font colors the right size upon resize (smaller than they are now)
+
+  // make the inputs have gradient backgrounds
+
+  // make the "session" and "39 minutes remain" have gradient text!!
+
+  // get https://codepen.io/JoeWeaver/pen/MQgWva code to draw a pink circle around session
+
+  // every second, pulse the window with 4 corner-originating gradient backgrounds
+  // http://www.colorzilla.com/gradient-editor/#ffb3ce+0,ffb3ce+100&1+0,0+28
+
+  // make input text color the same as the bottom border
+
+  // lots of other stuff
+
+});
