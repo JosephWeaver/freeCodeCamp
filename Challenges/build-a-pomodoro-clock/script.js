904db2c487ded160be2c33e87978747b14c1584b
JosephWeaver <joe.w.weaver@gmail.com>
2018-01-31 06:16:24 -0800

    Updated HTML, SCSS, & JS

--- Challenges/build-a-pomodoro-clock/script.js
+++ Challenges/build-a-pomodoro-clock/script.js
@@ -1,154 +1,137 @@
 // https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // https://use.fontawesome.com/releases/v5.0.6/js/all.js
 
-$(()=>{
+$(() => {
 
-  console.clear();
-
-  let $sessionInput = $("#sessionInput"),
-      $incrSession = $("#incrSession"),
-      $decrSession = $("#decrSession"),
-      $breakInput = $("#breakInput"),
-      $incrBreak = $("#incrBreak"),
-      $decrBreak = $("#decrBreak"),
+  let $audio = $("audio"), // from https://tide.moreless.io/en/
+      $theme = $(".theme"),
+      $title = $("#title"),
       $controls = $("#controls"),
       $options = $("#options"),
       $minutes = $("#minutes"),
       $seconds = $("#seconds"),
-      $title = $("#title"),
       $start = $("#start"),
       $pause = $("#pause"),
       $reset = $("#reset"),
-      breakLength = 5,
+      $incrSession = $("#incrSession"),
+      $sessionInput = $("#sessionInput"),
+      $decrSession = $("#decrSession"),
+      $incrBreak = $("#incrBreak"),
+      $breakInput = $("#breakInput"),
+      $decrBreak = $("#decrBreak"),
+      breakLength = 5 * 60,
       breakMax = 10,
       breakMin = 1,
-      breakTimeLeft = null,
-      currentBreak, // interval + function
-      currentSession,  // interval + function
-      sessionLength = 30,
+      sessionLength = 30 * 60,
       sessionMax = 60,
       sessionMin = 5,
-      speed = 1000,
-      timeLeft = null,
-      isBreaktime = false,
-      isCountdown = false;
+      sessionNum = 0,
+      countdown,
+      countType,
+      remainingTime = sessionLength;
 
   init();
 
   function init(){
-    $incrSession.click(()=>incrSession());
-    $decrSession.click(()=>decrSession());
-    $incrBreak.click(()=>incrBreak());
-    $decrBreak.click(()=>decrBreak());
-    $sessionInput.on("change", e=>updateSession(e.target.value));
-    $breakInput.on("change", e=>updateBreak(e.target.value));
-    $start.click(()=>{
-      if (isBreaktime === false){ startSession(timeLeft); }
-      if (isCountdown === false){ startBreak(breakTimeLeft); }
-    });
-    $pause.click(()=>pause());
-    $reset.click(()=>reset());
+    $audio.prop("volume", 0);
+    $incrSession.click(() => incrSession());
+    $decrSession.click(() => decrSession());
+    $incrBreak.click(() => incrBreak());
+    $decrBreak.click(() => decrBreak());
+    $sessionInput.on("change", e => updateSession(e.target.value));
+    $breakInput.on("change", e => updateBreak(e.target.value));
+    $start.click(() => { if (countType === "break"){ startBreak(); } else { startSession(); } });
+    $pause.click(() => pause());
+    $reset.click(() => reset());
+    $theme.click(e => audioSelect(e));
   }
-
-  function startSession(timeLeft = null){
-    isCountdown = true;
-    isBreaktime = false;
-    $options.slideUp();
+  function startSession(){
+    sessionNum++;
+    countType = "session";
+    $options.slideUp(143);
     $controls.removeClass().addClass("started");
     $title.fadeOut(43, function(){
-      $(this).html("Session").fadeIn();
+      $(this).html("Session " + sessionNum).fadeIn();
     });
-    if (timeLeft === null){ timeLeft = sessionLength * 60; }
-    currentSession = setInterval(() => {
-      timeLeft--;
-      let minLeft = Math.floor(timeLeft / 60);
-      let secLeft = timeLeft - minLeft * 60;
-      if (timeLeft < 1) {
-        startBreak(breakLength);
-        clearInterval(currentSession);
-      }
-      if (isCountdown === true){
-        $minutes.html(minLeft);
-        $seconds.html(secLeft < 10 ? "0" + secLeft : secLeft);
-      }
-    }, speed);
+    $audio.animate({volume: 1}, 1000);
+    start(remainingTime || sessionLength);
   }
-  function startBreak(breakTimeLeft = null){
-    isCountdown = false;
-    isBreaktime = true;
+  function startBreak(){
+    countType = "break";
     $title.fadeOut(43, function(){
-      $(this).html("Break").fadeIn();
+      $(this).html("Break " + sessionNum).fadeIn();
     });
-    if (breakTimeLeft < 10) { breakTimeLeft = breakLength * 60; }
-    currentBreak = setInterval(() => {
-      breakTimeLeft--;
-      let minLeft = Math.floor(breakTimeLeft / 60);
-      let secLeft = breakTimeLeft - minLeft * 60;
-      if (breakTimeLeft < 1) {
-        startSession(sessionLength);
-        clearInterval(currentBreak);
+    $audio.animate({volume: 0}, 5000);
+    start(remainingTime || breakLength);
+  }
+  function start(timeLeft){
+    clearInterval(countdown);
+    countdown = setInterval(() => {
+      timeLeft--;
+      remainingTime = timeLeft;
+      let minLeft = Math.floor(timeLeft / 60),
+          secLeft = timeLeft - minLeft * 60;
+      updateMinutes(minLeft);
+      updateSeconds(secLeft < 10 ? "0" + secLeft : secLeft);
+      if (timeLeft < 1){
+        if (countType === "session"){
+          startBreak(breakLength);
+        } else {
+          startSession();
+        }
       }
-      if (isBreaktime === true){
-        $minutes.html(minLeft);
-        $seconds.html(secLeft < 10 ? "0" + secLeft : secLeft);
-      }
-    }, speed);
+    }, 1000);
   }
   function pause(){
-    if (isCountdown === true) { clearInterval(currentSession); }
-    if (isBreaktime === true) { clearInterval(currentBreak); }
-    isCountdown = false;
-    isBreaktime = false;
-    $options.slideDown();
-    $reset.slideDown();
+    sessionNum--;
+    $audio.animate({volume: 0}, 1000);
+    clearInterval(countdown);
+    $options.slideDown(143);
     $controls.removeClass().addClass("paused");
     $title.fadeOut(43, function(){
       $(this).html("Paused").fadeIn();
     });
   }
-
   function reset(){
-    isCountdown = false;
-    isBreaktime = false;
-    $options.slideDown();
-    $reset.slideUp();
-    if (currentSession){ clearInterval(currentSession); }
-    if (currentBreak){ clearInterval(currentBreak); }
+    clearInterval(countdown);
+    updateMinutes(sessionLength / 60);
+    updateSeconds("00");
+    countType = undefined;
     $controls.removeClass().addClass("reset");
     $title.html("Ready?");
-    updateMinutes(sessionLength);
-    updateSeconds("00");
+    remainingTime = sessionLength;
   }
-
   function incrSession(){
-    reset();
     let num = Number($sessionInput.val());
     num = num + (num === sessionMax ? 0 : 1);
-    sessionLength = num;
+    sessionLength = num * 60;
     updateSession(num);
     updateMinutes(num);
     updateSeconds("00");
+    reset();
   }
   function decrSession(){
-    reset();
     let num = Number($sessionInput.val());
     num = num - (num === sessionMin ? 0 : 1);
-    sessionLength = num;
+    sessionLength = num * 60;
     updateSession(num);
     updateMinutes(num);
     updateSeconds("00");
+    reset();
   }
   function incrBreak(){
-    reset();
     let num = Number($breakInput.val());
     num = num + (num === breakMax ? 0 : 1);
+    breakLength = num * 60;
     updateBreak(num);
+    reset();
   }
   function decrBreak(){
-    reset();
     let num = Number($breakInput.val());
     num = num - (num === breakMin ? 0 : 1);
+    breakLength = num * 60;
     updateBreak(num);
+    reset();
   }
   function updateMinutes(num){
     $minutes.text(num);
@@ -157,10 +140,28 @@ $(()=>{
     $seconds.text(num);
   }
   function updateSession(num){
-    $sessionInput.val(num < sessionMin ? sessionMin : num > sessionMax ? sessionMax : num).blur();
+    num = num < sessionMin ? sessionMin : num > sessionMax ? sessionMax : num;
+    $sessionInput.val(num).blur();
+    updateMinutes(num);
+    updateSeconds("00");
+    sessionLength = num * 60;
+    reset();
   }
   function updateBreak(num){
     $breakInput.val(num < breakMin ? breakMin : num > breakMax ? breakMax : num).blur();
+    breakLength = num * 60;
+    reset();
+  }
+  function audioSelect(e){
+    $theme.removeClass("selected");
+    $(e.target).addClass("selected");
+    switch(e.target.id){
+      case "forest": $audio.attr("src", "http://joeweaver.me/codepenassets/freecodecamp/challenges/build-a-pomodoro-clock/forest.mp3"); break;
+      case "ocean": $audio.attr("src", "http://joeweaver.me/codepenassets/freecodecamp/challenges/build-a-pomodoro-clock/ocean.mp3"); break;
+      case "rainy": $audio.attr("src", "http://joeweaver.me/codepenassets/freecodecamp/challenges/build-a-pomodoro-clock/rain.mp3"); break;
+      case "peace": $audio.attr("src", "http://joeweaver.me/codepenassets/freecodecamp/challenges/build-a-pomodoro-clock/peace.mp3"); break;
+      case "cafe": $audio.attr("src", "http://joeweaver.me/codepenassets/freecodecamp/challenges/build-a-pomodoro-clock/cafe.mp3"); break;
+    }
   }
 
 });
