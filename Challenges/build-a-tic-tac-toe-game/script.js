20ff26950d698e50381314b9dcfceb7b9bc655f7
JosephWeaver <joe.w.weaver@gmail.com>
2018-02-03 20:53:03 -0800

    Updated JS

--- Challenges/build-a-tic-tac-toe-game/script.js
+++ Challenges/build-a-tic-tac-toe-game/script.js
@@ -7,12 +7,24 @@ $(()=>{
   let angel = "😇", // angel (X)
       devil = "😈", // devil (O)
       $board = $("#board"),
+      boardState = {
+        square1: "empty",
+        square2: "empty",
+        square3: "empty",
+        square4: "empty",
+        square5: "empty",
+        square6: "empty",
+        square7: "empty",
+        square8: "empty",
+        square9: "empty"
+      },
       $choice = $(".choice"),
       $choose = $("#choose"),
       $message = $("#play h2"),
       $play = $("#play"),
       $square = $("#board div"),
-      side, // player's choice
+      player1side, // angel / devil
+      player2side, // devil / angel
       winningCombos = [
         [1, 2, 3],
         [4, 5, 6],
@@ -33,7 +45,7 @@ $(()=>{
   }
   function makeChoice(e){
     side = e.target.id;
-    yourTurn(side);
+    whoseTurn(side);
     $(e.target).addClass("selected");
     $choose.fadeOut(function(){
       $play.css("display", "flex").hide().fadeIn();
@@ -41,7 +53,6 @@ $(()=>{
     startGame();
   }
   function startGame(){
-    let board = "";
     $board.fadeIn();
     playMove();
   }
@@ -58,21 +69,21 @@ $(()=>{
   function turnChange(){
     side = side == "angel" ? "devil" : "angel";
   }
-  function yourTurn(side){
+  function whoseTurn(side){
     if (side == angel){
-      $message.html("<span class='" + side + "s'>" + cap(side) + "s</span> go first: Your turn!");
+      $message.html("<span class='" + side + "s'>" + cap(side) + "s</span> first &ndash; Your turn!");
     } else {
-      $message.html("<span class='" + side + "s'>" + cap(side) + "s</span> go first: Your turn!");
-      computerPlayMove
+      $message.html("<span class='" + side + "s'>" + cap(side) + "s</span> always go first! Hehe");
+      // computerPlayMove();
     }
   }
   function resetGame(){
     $square.html("<span>Play<br>Here</span>").removeClass("played angel devil");
-    side = undefined;
+    player1side = undefined;
   }
   function resetAll(){
     $square.html("<span>Play<br>Here</span>").removeClass("played angel devil");
-    side = undefined;
+    player1side = undefined;
   }
   function cap(str){
     return str.charAt(0).toUpperCase() + str.slice(1);
