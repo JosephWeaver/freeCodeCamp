635c811ec93434326d6020171f84b7ab2ac43e62
JosephWeaver <joe.w.weaver@gmail.com>
2018-02-03 20:42:33 -0800

    Updated JS

--- Challenges/build-a-tic-tac-toe-game/script.js
+++ Challenges/build-a-tic-tac-toe-game/script.js
@@ -4,42 +4,41 @@ $(()=>{
 
   console.clear();
 
-  let winningCombos = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
-  ],
+  let angel = "😇", // good girlz
+      devil = "😈", // bad boyz
       $board = $("#board"),
-      $square = $("#board div"),
+      $choice = $(".choice"),
       $choose = $("#choose"),
-      $choice = $("#choices div"),
+      $message = $("#play h2"),
       $play = $("#play"),
-      $message = $play.children("h2"),
-      angel = "😇",
-      devil = "😈",
-      side;
+      $square = $("#board div"),
+      side, // player to choose
+      winningCombos = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+      ];
 
   init();
 
   function init(){
-    $choose.fadeIn();
-    $choice.click(e => {
-      side = e.target.id;
-      yourTurn(side);
-      $(e.target).addClass("selected");
-      $choose.fadeOut(function(){
-        $play.css("display", "flex").hide().fadeIn();
-      });
-      startGame();
+    $("body").css("display", "flex").hide().fadeIn();
+    $choice.click(e => makeChoice(e));
+    $("body").dblclick(()=>resetGame());
+  }
+  function makeChoice(e){
+    side = e.target.id;
+    yourTurn(side);
+    $(e.target).addClass("selected");
+    $choose.fadeOut(function(){
+      $play.css("display", "flex").hide().fadeIn();
     });
-    $("body").dblclick(function(){
-      resetGame();
-    });
+    startGame();
   }
   function startGame(){
     let board = "";
@@ -48,8 +47,12 @@ $(()=>{
   }
   function playMove(){
     $square.on("click", function(e){
-      $(this).html(side == "angel" ? angel : devil).addClass("played").addClass(side == "angel" ? "angel" : "devil");
-    turnChange();
+      if (!$(this).hasClass("played")){
+        $(this).html(side == "angel" ? angel : devil)
+          .addClass(side == "angel" ? "angel" : "devil")
+          .addClass("played");
+        turnChange();
+      }
     });
   }
   function turnChange(){
@@ -59,7 +62,10 @@ $(()=>{
     $message.html("<span class='" + side + "s'>" + cap(side) + "s</span> go first: Your turn!");
   }
   function resetGame(){
-    $square.html("<span>Play<br>Here</span>").removeClass("played");
+    $square.html("<span>Play<br>Here</span>").removeClass("played angel devil");
+  }
+  function resetAll(){
+    $square.html("<span>Play<br>Here</span>").removeClass("played angel devil");
   }
   function cap(str){
     return str.charAt(0).toUpperCase() + str.slice(1);
