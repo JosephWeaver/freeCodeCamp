df53d4b5075ea047fd5c94d98662e28e0b0799d4
JosephWeaver <joe.w.weaver@gmail.com>
2018-02-03 21:47:58 -0800

    Updated JS

--- Challenges/build-a-tic-tac-toe-game/script.js
+++ Challenges/build-a-tic-tac-toe-game/script.js
@@ -23,8 +23,8 @@ $(()=>{
       $message = $("#play h2"),
       $play = $("#play"),
       $square = $(".square"),
-      player1side, // angel / devil
-      player2side, // devil / angel
+      p1side,
+      p2side,
       winningCombos = [
         [1, 2, 3],
         [4, 5, 6],
@@ -41,52 +41,55 @@ $(()=>{
   function init(){
     $("body").css("display", "flex").hide().fadeIn(); // show bodyy
     $("body").dblclick(() => resetGame()); // hidden reset function
-    $choice.click(e => makeChoice(e)); // player chooses their side
+    $choice.click(e => chooseSide(e)); // player chooses their side
   }
-  function makeChoice(e){
-    side = e.target.id;
-    whoseTurn(side);
+  function chooseSide(e){
+    p1side = e.target.id;
+    whoseTurn();
     $(e.target).addClass("selected");
     $choose.fadeOut(function(){
       $play.css("display", "flex").hide().fadeIn();
     });
     startGame();
   }
-  function startGame(){
-    $board.fadeIn();
-    playMove();
+  function computerPlay(){
+    // computer plays move
+  }
+  function someoneWon(){
+    // check win condition
+    return false;
   }
   function playMove(){
     $square.on("click", function(e){
       if (!$(this).hasClass("played")){
-        $(this).html(side == "angel" ? angel : devil)
-          .addClass(side == "angel" ? "angel" : "devil")
+        $(this).html(p1side == "angel" ? angel : devil)
+          .addClass(p1side == "angel" ? "angel" : "devil")
           .addClass("played");
-        turnChange();
+        if(someoneWon()){
+          winMessage();
+        } else {
+          turnChange();
+        }
       }
     });
   }
-  function turnChange(){
-    side = side == "angel" ? "devil" : "angel";
-  }
-  function whoseTurn(side){
-    if (side == angel){
-      $message.html("<span class='" + side + "s'>" + cap(side) + "s</span> first &ndash; Your turn!");
-    } else {
-      $message.html("<span class='" + side + "s'>" + cap(side) + "s</span> always go first! Hehe");
-      // computerPlayMove();
-    }
-  }
   function resetGame(){
-    $square.html("<span>Play<br>Here</span>").removeClass("played angel devil");
-    player1side = undefined;
+    $square.html("").removeClass("played angel devil");
+    p1side = undefined;
   }
-  function resetAll(){
-    $square.html("<span>Play<br>Here</span>").removeClass("played angel devil");
-    player1side = undefined;
+  function startGame(){
+    $board.fadeIn();
+    playMove();
   }
-  function cap(str){
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  function turnChange(){
+    p1side = p1side == "angel" ? "devil" : "angel";
+  }
+  function whoseTurn(){
+    $message.html(p1side == "angel" ? "<span class='angels'>Angels</span> first &ndash; your turn!" : "Sorry, <span class='angels'>Angels</span> always go first!");
+    computerPlayMove();
+  }
+  function winMessage(){
+    // show win message
   }
 
 });
